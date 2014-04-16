0d35275584203e9a73db24d74b519b1cbc680fdf
John Downs <jdowns@Johns-MacBook-Pro.local>
2014-04-15 22:53:14 -0400

    add index page

--- doh.py
+++ doh.py
@@ -4,23 +4,37 @@ import mongolab_cred as mc
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = "mongodb://%s:%s@ds029267.mongolab.com:29267/nyc_restaurants" % (mc.username, mc.password)
+uri = "mongodb://%s:%s@ds029267.mongolab.com:29267/nyc_restaurants"
+app.config['MONGO_URI'] = uri  % (mc.username, mc.password)
 
 mongo = PyMongo(app)
 
+def get_grades(zipcode):
+    match = {"$match": {"ZIPCODE": zipcode}}
+    groupby = {"$group":
+        {"_id":
+            {"camis": "$CAMIS", 
+             "dba": "$DBA", 
+             "grade": "$CURRENTGRADE"}, 
+         "RECORDDATE": {"$max":"$RECORDDATE"}
+        }
+     }
+    rest_in_zip = mongo.db.ratings.aggregate([match, groupby])
+    return rest_in_zip['result']
+
 @app.route("/")
 def hello():
-    return "NYC DOH STATS"
+    return render_template('index.html')
 
 @app.route("/stats/<int:zipcode>")
 def stats(zipcode):
-    rest_in_zip = mongo.db.ratings.find({'ZIPCODE': zipcode})
-    return str(list(rest_in_zip))
+    return str(get_grades(zipcode))
 
 @app.route("/view/<int:zipcode>")
 def stats_view(zipcode):
-    rest_in_zip = mongo.db.ratings.find({'ZIPCODE': zipcode}).sort("INSPDATE", -1)
-    return render_template('dataview.html', zipcode=zipcode, data=rest_in_zip)
+    return render_template('dataview.html', 
+                           zipcode=zipcode, 
+                           data=get_grades(zipcode))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
