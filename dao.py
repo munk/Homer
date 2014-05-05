3f38bb834c192f1654aaf792c321e27888632c4b
John <john.downs@acm.org>
2014-05-04 22:20:09 -0400

    added dao

---
+++ dao.py
@@ -0,0 +1,57 @@
+from cuisine import res
+
+class GradeDAO(object):
+    groupby = {"$group":
+        {"_id":
+            {"camis": "$CAMIS",
+             "dba": "$DBA",
+             "grade": "$CURRENTGRADE"},
+         "RECORDDATE": {"$max":"$RECORDDATE"}
+        }
+    }
+    borodict = {"manhattan": 1,
+              "brooklyn": 3,
+              "queens": 4,
+              "statenisland": 5,
+              "thebronx": 2}
+
+    def __init__(self, mongo):
+        self.ratings = mongo.db.ratings
+
+    def __get_grades__(self, match, groupby=groupby):
+        resultset = self.ratings.aggregate([match, groupby])
+        return resultset['result']
+
+    def get_grade_by_zipcode(self, key):
+        match = {"$match": {"ZIPCODE": key}}
+        return self.__get_grades__(match)
+
+    def get_grade_by_boro(self, key):
+        match = {"$match": {"BORO": boro[key]}}
+        return self.__get_grades__(match)
+
+    def get_grade_by_cuisine(self, key):
+        match = {"%match": {"CUISINE": key}}
+        return self.__get_grades__(match)
+
+    def get_grade_by_phone(self, key):
+        match = {"$match": {"PHONE": phone}}
+        groupby = {
+            "$group":{"_id":
+                         {"insp_id" : "$_id",
+                          "camis": "$CAMIS",
+                          "dba": "$DBA",
+                          "score": "$SCORE",
+                          "cuisine": "$CUISINECODE",
+                          "violation": "$VIOLCODE",
+                          "grade": "$CURRENTGRADE"},
+                    "INSPECTIONDATE": {"$max":"$INSPDATE"}}}
+        return self.__get_grades__(match, groupby)
+
+    def get_summary(self, boro, cuisine=False):
+        query = {'BORO': self.borodict[boro]}
+        if cuisine:
+            query['CUISINECODE'] = res[cuisine]
+        camis = self.ratings.find(query).distinct('CAMIS')
+        result = self.ratings.find({'CAMIS': {"$in": camis}})
+        return result
